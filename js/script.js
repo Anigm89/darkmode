1b8ff0e1fb0bb879e6e262a39cd164f02821cab5
Aniana <anigm89@gmail.com>
2023-12-04 14:46:34 +0100

    modo oscuro terminado

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const boton = document.getElementById('boton');
+const body = document.getElementById('body');
+const titulo = document.querySelector('.titulo');
+const text = document.querySelector('.text');
+const titular = document.querySelector('.titular');
+const textos= document.querySelectorAll('.texto')
+
+
+boton.addEventListener('click', () => {
+    
+   // $("#body").removeClass('blanco');	
+    //body.classList.remove('blanco');
+    //body.classList.add('negro');
+    body.classList.toggle("negro");
+    titulo.classList.toggle('amarillo');
+    text.classList.toggle('blanco');
+    boton.classList.toggle('azul')
+    titular.classList.toggle('amarillo');
+    textos.forEach(texto => {
+        texto.classList.toggle('blanco');
+    })
+
+
+    //animacion
+    boton.classList.toggle('mover');
+
+})
+
